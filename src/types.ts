d9f66e85ea0ddf40c9ef84d01322f0f7c200103f
evgnyx <se.ev@ya.ru>
2021-01-11 16:32:40 +0300

    update

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
-export type Fn = (...args: any[]) => any
+export type Fn = (value?: any, ...args: any[]) => any
 export type ClassList = { [className: string]: string }
 export interface Config { [name: string]: ClassList }
