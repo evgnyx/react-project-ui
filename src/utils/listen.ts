73103f2140ec0fcc430e2550c81935d49f1930bd
evgnyx <se.ev@ya.ru>
2021-01-11 16:34:24 +0300

    update

--- src/utils/listen.ts
+++ src/utils/listen.ts
@@ -1,8 +1,15 @@
 import { Fn } from '../types'
 
-export default function listen(target: any, events: string[], handler: Fn): () => void {
-  events.forEach((e) => target.addEventListener(e, handler))
+export default function listen(
+  target: HTMLElement | (Window & typeof globalThis) | Document,
+  events: string | string[],
+  handler: Fn
+): () => void {
+  if (typeof events === 'string') target.addEventListener(events, handler)
+  else events.forEach((e) => target.addEventListener(e, handler))
+
   return () => {
-    events.forEach((e) => target.removeEventListener(e, handler))
+    if (typeof events === 'string') target.removeEventListener(events, handler)
+    else events.forEach((e) => target.removeEventListener(e, handler))
   }
 }
