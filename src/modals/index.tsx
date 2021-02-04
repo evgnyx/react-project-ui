88b88d0d84e25097ddc7807283b54980c458cc89
evgnyx <se.ev@ya.ru>
2021-02-04 13:54:58 +0300

    update

--- src/modals/index.tsx
+++ src/modals/index.tsx
@@ -1,2 +1,5 @@
-export { default } from './modals'
-export { default as useModal } from './use-modal'
+import Modals from './modals'
+import useModal from './use-modal'
+
+export { useModal }
+export default Modals
